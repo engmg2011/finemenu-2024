d3720341c5802d32b1159524e4fab3cd95356880
Mohamed Gamal <Eng.mg2011@gmail.com>
2024-06-08 15:30:43 +0300

    separated restaurant routes

--- routes/api.php
+++ routes/api.php
@@ -85,54 +85,6 @@ Route::group(['middleware' => [
         Route::get('/{id}/settings/{settingId}/delete', [SettingsController::class, 'deleteSetting']);
     });
 
-    Route::group(['prefix' => 'restaurants', 'middleware' => [SetRequestModel::class]], function () {
-        Route::get('/', [RestaurantsController::class, 'index']);
-        Route::get('/{id}', [RestaurantsController::class, 'show']);
-        Route::post('/', [RestaurantsController::class, 'create']);
-        Route::post('/{id}', [RestaurantsController::class, 'update']);
-        Route::post('/{id}/delete', [RestaurantsController::class, 'destroy']);
-        Route::get('/{id}/menus', [RestaurantsController::class, 'restaurantMenus']);
-
-
-        // Restaurant branches
-        Route::group(['prefix' => '/{restaurantId}/branches'], function () {
-            Route::get('/', [BranchesController::class, 'index']);
-            Route::get('/{id}', [BranchesController::class, 'show']);
-            Route::post('/', [BranchesController::class, 'createModel']);
-            Route::post('/{id}/delete', [BranchesController::class, 'destroy']);
-            Route::post('/{id}', [BranchesController::class, 'update']);
-            Route::post('/sort', [BranchesController::class, 'sort']);
-
-            // Restaurant Branch floors
-            Route::group(['prefix' => '/{branchId}/floors'], function () {
-                Route::get('/', [FloorsController::class, 'index']);
-                Route::get('/{id}', [FloorsController::class, 'show']);
-                Route::post('/', [FloorsController::class, 'createModel']);
-                Route::post('/{id}/delete', [FloorsController::class, 'destroy']);
-                Route::post('/{id}', [FloorsController::class, 'update']);
-
-                // Restaurant Branch floors tables
-                Route::group(['prefix' => '/{floorId}/tables'], function () {
-                    Route::get('/', [TablesController::class, 'index']);
-                    Route::get('/{id}', [TablesController::class, 'show']);
-                    Route::post('/', [TablesController::class, 'createModel']);
-                    Route::post('/{id}/delete', [TablesController::class, 'destroy']);
-                    Route::post('/{id}', [TablesController::class, 'update']);
-                });
-
-            });
-        });
-
-
-        Route::get('/{id}/settings', [SettingsController::class, 'listSettings']);
-        Route::post('/{id}/settings', [SettingsController::class, 'createSetting']);
-        Route::post('/{id}/settings/{settingId}', [SettingsController::class, 'updateSetting']);
-        Route::get('/{id}/settings/{settingId}/delete', [SettingsController::class, 'deleteSetting']);
-        Route::get('/{id}/orders', [OrdersController::class, 'restaurantOrders']);
-    });
-
-
-
     Route::group(['prefix' => 'contacts'], function () {
         Route::get('/', [ContactController::class, 'index']);
         Route::get('/{id}', [ContactController::class, 'show']);
@@ -297,16 +249,6 @@ Route::group(['middleware' => [
         Route::post('/', [DietPlansController::class, 'create']);
         Route::post('/{id}', [DietPlansController::class, 'update']);
         Route::get('/{id}/delete', [DietPlansController::class, 'destroy']);
-    });
-
-
-
-    Route::group(['prefix' => 'menus'], function () {
-        Route::get('/', [MenusController::class, 'index']);
-        Route::get('/{id}', [MenusController::class, 'show']);
-        Route::post('/', [MenusController::class, 'createModel']);
-        Route::post('/{id}/delete', [MenusController::class, 'destroy']);
-        Route::post('/{id}', [MenusController::class, 'update']);
     });
 
 
---
+++ routes/restaurant_apis.php
@@ -0,0 +1,79 @@
+<?php
+
+use App\Constants\RolesConstants;
+use App\Http\Controllers\BranchesController;
+use App\Http\Controllers\FloorsController;
+use App\Http\Controllers\MenusController;
+use App\Http\Controllers\OrdersController;
+use App\Http\Controllers\RestaurantsController;
+use App\Http\Controllers\SettingsController;
+use App\Http\Controllers\TablesController;
+use App\Http\Middleware\SetRequestModel;
+use Illuminate\Support\Facades\Route;
+
+
+// TODO :: put admin only roles
+Route::group(['middleware' => [
+    'auth:api',
+    'role:' . RolesConstants::ADMIN . '|' . RolesConstants::OWNER]
+], function () {
+
+    Route::group(['prefix' => 'restaurants', 'middleware' => [SetRequestModel::class]], function () {
+        Route::get('/', [RestaurantsController::class, 'index']);
+        Route::get('/{id}', [RestaurantsController::class, 'show']);
+        Route::post('/', [RestaurantsController::class, 'create']);
+        Route::post('/{id}', [RestaurantsController::class, 'update']);
+        Route::post('/{id}/delete', [RestaurantsController::class, 'destroy']);
+        Route::get('/{id}/menus', [RestaurantsController::class, 'restaurantMenus']);
+
+        Route::get('/{modelId}/settings', [SettingsController::class, 'listSettings']);
+        Route::post('/{modelId}/settings/set', [SettingsController::class, 'setSetting']);
+        Route::post('/{modelId}/settings', [SettingsController::class, 'createSetting']);
+        Route::post('/{modelId}/settings/{settingId}', [SettingsController::class, 'updateSetting']);
+        Route::get('/{modelId}/settings/{settingId}/delete', [SettingsController::class, 'deleteSetting']);
+
+        Route::get('/{id}/orders', [OrdersController::class, 'restaurantOrders']);
+
+        // Restaurant branches
+        Route::group(['prefix' => '/{restaurantId}/branches'], function () {
+            Route::get('/', [BranchesController::class, 'index']);
+            Route::get('/{id}', [BranchesController::class, 'show']);
+            Route::post('/', [BranchesController::class, 'createModel']);
+            Route::post('/{id}/delete', [BranchesController::class, 'destroy']);
+            Route::post('/{id}', [BranchesController::class, 'update']);
+            Route::post('/sort', [BranchesController::class, 'sort']);
+
+            // Restaurant Branch floors
+            Route::group(['prefix' => '/{branchId}/floors'], function () {
+                Route::get('/', [FloorsController::class, 'index']);
+                Route::get('/{id}', [FloorsController::class, 'show']);
+                Route::post('/', [FloorsController::class, 'createModel']);
+                Route::post('/{id}/delete', [FloorsController::class, 'destroy']);
+                Route::post('/{id}', [FloorsController::class, 'update']);
+
+                // Restaurant Branch floors tables
+                Route::group(['prefix' => '/{floorId}/tables'], function () {
+                    Route::get('/', [TablesController::class, 'index']);
+                    Route::get('/{id}', [TablesController::class, 'show']);
+                    Route::post('/', [TablesController::class, 'createModel']);
+                    Route::post('/{id}/delete', [TablesController::class, 'destroy']);
+                    Route::post('/{id}', [TablesController::class, 'update']);
+                });
+
+            });
+        });
+
+
+
+        Route::group(['prefix' => 'menus'], function () {
+            Route::get('/', [MenusController::class, 'index']);
+            Route::get('/{id}', [MenusController::class, 'show']);
+            Route::post('/', [MenusController::class, 'createModel']);
+            Route::post('/{id}/delete', [MenusController::class, 'destroy']);
+            Route::post('/{id}', [MenusController::class, 'update']);
+        });
+
+
+    });
+
+});
