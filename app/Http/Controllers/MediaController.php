b78076491e2b7e5b14177bb4541dff53a8cb520c
Mohamed Gamal <Eng.mg2011@gmail.com>
2024-06-20 11:30:44 +0300

    Hotels fix

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -88,6 +88,7 @@ class MediaController extends Controller
         if ($this->request->headers->has('convert-item')) {
             $fullPath = $this->request->header('full-path');
 
+
             $myFile = [
                 'fullPath' => $fullPath,
                 'uploadedFilePath' => $storagePath,
