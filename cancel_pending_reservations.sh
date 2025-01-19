23a90da422d1291c14f45da67d014eff220f2fcb
Mohamed Gamal <Eng.mg2011@gmail.com>
2025-01-19 18:04:51 +0300

    Updated shell file

--- cancel_pending_reservations.sh
+++ cancel_pending_reservations.sh
@@ -2,10 +2,6 @@
 # Navigate to the project directory
 cd /var/www/menuai/backend
 
-echo "running";
-
-
-
 # Run the CancelPendingReservations command using Sail
 ./vendor/bin/sail artisan app:cancel-pending-reservations
 
