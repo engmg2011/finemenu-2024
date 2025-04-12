d038580b392d51451361996b1900696d4dbe1681
Mohamed Gamal <Eng.mg2011@gmail.com>
2025-04-12 17:20:28 +0300

    update cashed and notify after cancelled notifications

--- app/Console/Commands/CancelPendingReservations.php
+++ app/Console/Commands/CancelPendingReservations.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Constants\PaymentConstants;
+use App\Events\UpdateReservation;
 use App\Models\Reservation;
 use Illuminate\Console\Command;
 
@@ -28,9 +29,19 @@ class CancelPendingReservations extends Command
     public function handle()
     {
         try {
+            $reservationIds = Reservation::where('created_at', '<', now()->subMinutes(5))
+                    ->where('status', PaymentConstants::RESERVATION_PENDING)
+                    ->pluck('id');
+
             Reservation::where('created_at', '<', now()->subMinutes(5))
                 ->where('status', PaymentConstants::RESERVATION_PENDING)
                 ->update(['status' => PaymentConstants::RESERVATION_CANCELED]);
+
+            foreach ($reservationIds as $reservationId) {
+                app('App\Repository\Eloquent\ReservationRepository')->setReservationCashedData($reservationId);
+                event(new UpdateReservation($reservationId));
+            }
+
         }catch (\Exception $e){
             echo $e->getMessage();
             \Log::error("checking pending reservations -  " . $e->getMessage());
