48a7530201ee9cd3b555a31a2fd4a5c9b89393b4
Mohamed Gamal <Eng.mg2011@gmail.com>
2025-05-13 13:35:54 +0300

    cron wrapper update

--- cron-wrapper.php
+++ cron-wrapper.php
@@ -1,6 +1,6 @@
 <?php
 
 for ($i = 0; $i < 12; $i++) {
-    exec('/usr/local/bin/php /home1/psklwqte/public_html/backend/artisan schedule:run');
+    exec('/usr/local/bin/php /home1/psklwqte/public_html/backend/artisan queue:work');
     sleep(5);
 }
