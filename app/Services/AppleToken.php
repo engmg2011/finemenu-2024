851a0c59e89e602d3e4334de837575d1dd9dc370
Mohamed Gamal <Eng.mg2011@gmail.com>
2025-10-02 16:40:00 +0300

    exception handler for not found apple key

--- app/Services/AppleToken.php
+++ app/Services/AppleToken.php
@@ -8,17 +8,22 @@ class AppleToken
 {
     public static function generate()
     {
-        $keyFile = storage_path('Keys/Apple/AuthKey_'.env('APPLE_KEY_ID').'.p8');
-        $privateKey = file_get_contents($keyFile);
+        try {
+            $keyFile = storage_path('app/Keys/Apple/AuthKey_' . env('APPLE_KEY_ID') . '.p8');
+            $privateKey = file_get_contents($keyFile);
+            $claims = [
+                'iss' => env('APPLE_TEAM_ID'),
+                'iat' => time(),
+                'exp' => time() + 86400 * 180, // 6 months
+                'aud' => 'https://appleid.apple.com',
+                'sub' => env('APPLE_CLIENT_ID'),
+            ];
 
-        $claims = [
-            'iss' => env('APPLE_TEAM_ID'),
-            'iat' => time(),
-            'exp' => time() + 86400*180, // 6 months
-            'aud' => 'https://appleid.apple.com',
-            'sub' => env('APPLE_CLIENT_ID'),
-        ];
+            return JWT::encode($claims, $privateKey, 'ES256', env('APPLE_KEY_ID'));
 
-        return JWT::encode($claims, $privateKey, 'ES256', env('APPLE_KEY_ID'));
+        } catch (\Exception $e) {
+            \Log::error('Could not generate apple token');
+        }
+        return '';
     }
 }
