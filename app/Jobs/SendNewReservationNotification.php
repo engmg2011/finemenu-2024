d32abd7523625f7f1d82a9b5e9cc17cafe4a54b7
Mohamed Gamal <Eng.mg2011@gmail.com>
2025-12-06 19:00:04 +0300

    Updated notification messages

--- app/Jobs/SendNewReservationNotification.php
+++ app/Jobs/SendNewReservationNotification.php
@@ -5,6 +5,7 @@ namespace App\Jobs;
 use App\Models\Business;
 use App\Models\Reservation;
 use App\Services\NotificationService;
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -34,7 +35,11 @@ class SendNewReservationNotification implements ShouldQueue
         $branchName = $this->reservation->branch->locales[0]->name ?? "";
         $msg = [];
         $msg['subject'] = $branchName ?? "BarqSolutions";
-        $msg['message'] = "Booking $firstItemName from $branchName ";
+        $msg['message'] = "Booking $firstItemName from $branchName ".
+            ', ['. Carbon::parse($this->reservation->from)->format('d-m-y')
+            . ' - '. Carbon::parse($this->reservation->to)->format('d-m-y'). ' ] ,'.
+            'Unit '. $this->reservation->unit . ', Status '. $this->reservation->status.
+            ( $this->reservation->order_id ? ' 📱': '');
         return $msg;
     }
 
--- app/Jobs/SendUpdateReservationNotification.php
+++ app/Jobs/SendUpdateReservationNotification.php
@@ -6,6 +6,7 @@ use App\Constants\PaymentConstants;
 use App\Models\Business;
 use App\Models\Reservation;
 use App\Services\NotificationService;
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -36,7 +37,11 @@ class SendUpdateReservationNotification implements ShouldQueue
         $msg = [];
         $msg['subject'] = $branchName ?? "BarqSolutions";
         $title = $this->type === PaymentConstants::RESERVATION_CANCELED ? "Canceled" : "Updated";
-        $msg['message'] = $title . " booking for $firstItemName from $branchName Booking ID " . $this->reservation->id;
+        $msg['message'] = $title . " booking for $firstItemName, Booking ID " . $this->reservation->id.
+            ', ['. Carbon::parse($this->reservation->from)->format('d-m-y')
+            . ' - '. Carbon::parse($this->reservation->to)->format('d-m-y'). ' ] ,'.
+            'Unit '. $this->reservation->unit . ', Status '. $this->reservation->status.
+            ( $this->reservation->order_id ? ' 📱': '');
         return $msg;
     }
 
