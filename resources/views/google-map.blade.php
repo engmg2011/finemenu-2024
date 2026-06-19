7101293f1804c357addfd1b6ebd49b605fc7850b
Mohamed Gamal <Eng.mg2011@gmail.com>
2026-06-19 18:38:22 +0300

    Added marker to map

--- resources/views/google-map.blade.php
+++ resources/views/google-map.blade.php
@@ -1,3 +1,4 @@
 <img alt="Google Map image"
-    src="https://maps.googleapis.com/maps/api/staticmap?center={{
-    $lat }},{{ $lng }}&zoom={{ $zoom }}&size={{ $size }}&markers=color:red|29.3759,47.9774&key={{ $apiKey }}">
+     src="https://maps.googleapis.com/maps/api/staticmap?center={{
+    $lat }},{{ $lng }}&zoom={{ $zoom }}&size={{ $size }}&markers=color:red%7Clabel:S%7C{{
+    $lat }},{{ $lng }}&maptype=roadmap&key={{ $apiKey }}">
