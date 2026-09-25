464b2acfb2bc352f3e7836c88c961099e9417aa7
Mohamed Gamal <Eng.mg2011@gmail.com>
2026-09-25 19:38:05 +0300

    returned coupon data with reservations list

--- app/Repository/Eloquent/ReservationRepository.php
+++ app/Repository/Eloquent/ReservationRepository.php
@@ -113,7 +113,8 @@ class ReservationRepository extends BaseRepository implements ReservationReposit
         $endDate = businessToUtcConverter($data['to'], $business, 'Y-m-d H:i:s');
 
         // TODO :: agree on default
-        $reservations = Reservation::where(['branch_id' => $branchId, 'business_id' => $businessId])
+        $reservations = Reservation::with(['order:id,coupon_id,coupon_data,discount_amount,total_price,subtotal_price', 'order.coupon:id,code,discount_type,discount_value'])
+            ->where(['branch_id' => $branchId, 'business_id' => $businessId])
             ->whereHas('reservable')
             ->where(function ($query) use ($itemId, $status, $reservedForId, $reservedById, $followerId) {
                 if (isset($itemId)) $query->where('reservable_id', $itemId);
@@ -148,7 +149,11 @@ class ReservationRepository extends BaseRepository implements ReservationReposit
 
     public function listModel($businessId, $branchId, $conditions = null)
     {
-        return Reservation::with('reservable.featuredImage')
+        return Reservation::with([
+                'reservable.featuredImage',
+                'order:id,coupon_id,coupon_data,discount_amount,total_price,subtotal_price',
+                'order.coupon:id,code,discount_type,discount_value',
+            ])
             ->where(['branch_id' => $branchId, 'business_id' => $businessId])
             ->where(fn($q) => $conditions ? $q->where(...$conditions) : $q)
             ->orderByDesc('id')
